d496a7c21ace47ca699ae456692eb68f9ca18ab4
super Harry <harry-run@yandex.ru>
2021-12-11 16:46:15 +0300

    Header

---
+++ src/pages/FoodMenu/components/MenuBlock.tsx
@@ -0,0 +1,119 @@
+import React from "react";
+
+interface Props {}
+
+export const MenuBlock = (props: Props) => {
+  return (
+    <section id="ourPakeg" className="ourPakeg">
+      <div className="container">
+        <div className="main_pakeg_content">
+          <div className="row">
+            <div className="head_title text-center">
+              <h4>Amazing</h4>
+              <h3>Delicious</h3>
+            </div>
+
+            <div className="single_pakeg_one text-right wow rotateInDownRight">
+              <div className="col-md-6 col-md-offset-6 col-sm-8 col-sm-offset-4">
+                <div className="single_pakeg_text">
+                  <div className="pakeg_title">
+                    <h4>Drinks</h4>
+                  </div>
+
+                  <ul>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="single_pakeg_two text-left wow rotateInDownLeft">
+              <div className="col-md-6 col-sm-8">
+                <div className="single_pakeg_text">
+                  <div className="pakeg_title">
+                    <h4>Main course </h4>
+                  </div>
+
+                  <ul>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="single_pakeg_three text-left wow rotateInDownRight">
+              <div className="col-md-6 col-md-offset-6 col-sm-8 col-sm-offset-4">
+                <div className="single_pakeg_text">
+                  <div className="pakeg_title">
+                    <h4>Desserts</h4>
+                  </div>
+
+                  <ul>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                    <li>
+                      Tuna Roast
+                      Source........................................................$24.5
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/pages/FoodMenu/components/Portfolio.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+
+interface Props {}
+
+export const Portfolio = (props: Props) => {
+  return (
+    <div>
+      <section id="portfolio" className="portfolio">
+        <div className="container">
+          <div className="row">
+            <div
+              className="portfolio_content text-center  wow fadeIn"
+              data-wow-duration="5s"
+            >
+              <div className="col-md-12">
+                <div className="head_title text-center">
+                  <h4>Delightful</h4>
+                  <h3>Experience</h3>
+                </div>
+
+                <div className="main_portfolio_content">
+                  <div className="col-md-3 col-sm-4 col-xs-6 single_portfolio_text">
+                    <img src="assets/images/p1.png" alt="" />
+                    <div className="portfolio_images_overlay text-center">
+                      <h6>Italian Source Mushroom</h6>
+                      <p className="product_price">$12</p>
+                      <a href="/test" className="btn btn-primary">
+                        Click here
+                      </a>
+                    </div>
+                  </div>
+                  <div className="col-md-3 col-sm-4 col-xs-6 single_portfolio_text">
+                    <img src="assets/images/p2.png" alt="" />
+                    <div className="portfolio_images_overlay text-center">
+                      <h6>Italian Source Mushroom</h6>
+                      <p className="product_price">$12</p>
+                      <a href="/test" className="btn btn-primary">
+                        Click here
+                      </a>
+                    </div>
+                  </div>
+                  <div className="col-md-3 col-sm-4 col-xs-6 single_portfolio_text">
+                    <img src="assets/images/p3.png" alt="" />
+                    <div className="portfolio_images_overlay text-center">
+                      <h6>Italian Source Mushroom</h6>
+                      <p className="product_price">$12</p>
+                      <a href="/test" className="btn btn-primary">
+                        Click here
+                      </a>
+                    </div>
+                  </div>
+                  <div className="col-md-3 col-sm-4 col-xs-6 single_portfolio_text">
+                    <img src="assets/images/p4.png" alt="" />
+                    <div className="portfolio_images_overlay text-center">
+                      <h6>Italian Source Mushroom</h6>
+                      <p className="product_price">$12</p>
+                      <a href="/test" className="btn btn-primary">
+                        Click here
+                      </a>
+                    </div>
+                  </div>
+                  <div className="col-md-3 col-sm-4 col-xs-6 single_portfolio_text">
+                    <img src="assets/images/p5.png" alt="" />
+                    <div className="portfolio_images_overlay text-center">
+                      <h6>Italian Source Mushroom</h6>
+                      <p className="product_price">$12</p>
+                      <a href="/test" className="btn btn-primary">
+                        Click here
+                      </a>
+                    </div>
+                  </div>
+                  <div className="col-md-3 col-sm-4 col-xs-6 single_portfolio_text">
+                    <img src="assets/images/p6.png" alt="" />
+                    <div className="portfolio_images_overlay text-center">
+                      <h6>Italian Source Mushroom</h6>
+                      <p className="product_price">$12</p>
+                      <a href="/test" className="btn btn-primary">
+                        Click here
+                      </a>
+                    </div>
+                  </div>
+                  <div className="col-md-3 col-sm-4 col-xs-6 single_portfolio_text">
+                    <img src="assets/images/p7.png" alt="" />
+                    <div className="portfolio_images_overlay text-center">
+                      <h6>Italian Source Mushroom</h6>
+                      <p className="product_price">$12</p>
+                      <a href="/test" className="btn btn-primary">
+                        Click here
+                      </a>
+                    </div>
+                  </div>
+                  <div className="col-md-3 col-sm-4 col-xs-6 single_portfolio_text">
+                    <img src="assets/images/p8.png" alt="" />
+                    <div className="portfolio_images_overlay text-center">
+                      <h6>Italian Source Mushroom</h6>
+                      <p className="product_price">$12</p>
+                      <a href="/test" className="btn btn-primary">
+                        Click here
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
