fedec1b322c90e8fe4bd52df08c814fd03ca182a
super Harry <harry-run@yandex.ru>
2021-12-23 20:49:39 +0300

    Fix

--- backend/src/entities/food-menu.entity.ts
+++ backend/src/entities/food-menu.entity.ts
@@ -1,1 +1,26 @@
-export class FoodMenu {}
+import { Exclude } from 'class-transformer';
+import { Column, Entity, Index, PrimaryColumn } from 'typeorm';
+@Entity()
+export class FoodMenuEntity {
+    @Column()
+    @PrimaryColumn()
+    @Index({ unique: true })
+    userId: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt?: Date;
+  
+    @Exclude()
+    @Column({
+      type: 'timestamp',
+      default: () => 'CURRENT_TIMESTAMP',
+      onUpdate: 'CURRENT_TIMESTAMP',
+    })
+    updatedAt?: Date;
+}
