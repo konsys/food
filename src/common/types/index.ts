f1659faa94b0050d85e67348459c7b26cbaae0af
super Harry <harry-run@yandex.ru>
2022-01-13 20:00:33 +0300

    Fix

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -3,3 +3,24 @@ export type TLoadById = (id: number | string) => void;
 export type TVoidFn<T extends any = void> = (arg: T) => void;
 export type TPromiseFn<T extends any = void, R = void> = (arg: T) => Promise<R>;
 export type TOnlyView = { onlyView: boolean };
+
+export type TFileUpload = {
+  averageImg: string | null;
+  bigImg: string | null;
+  createdAt: Date;
+  description: string | null;
+  destination: string;
+  encoding: string;
+  fieldname: string;
+  filename: string;
+  id: number;
+  mimetype: string;
+  name: string;
+  original: string;
+  originalname: string;
+  path: string;
+  size: number;
+  smallImg: string | null;
+  updatedAt: Date;
+  visible: false;
+};
