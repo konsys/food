0795cb012acd155fd9268b48c206932c78364476
super Harry <harry-run@yandex.ru>
2022-02-03 22:10:52 +0300

    wefwef

--- src/common/template/Header/Header.tsx
+++ src/common/template/Header/Header.tsx
@@ -1,26 +1,54 @@
-import { Col, Row } from 'antd';
+import { Button, Col, Row } from 'antd';
 import React, { FC } from 'react';
 import './styles.less';
 
 export const Header: FC = () => (
-  <>
-    <Row>
-      <Col span={24}>col</Col>
-    </Row>
-    <Row>
-      <Col span={12}>col-12</Col>
-      <Col span={12}>col-12</Col>
-    </Row>
-    <Row>
-      <Col span={8}>col-8</Col>
-      <Col span={8}>col-8</Col>
-      <Col span={8}>col-8</Col>
-    </Row>
-    <Row>
-      <Col span={6}>col-6</Col>
-      <Col span={6}>col-6</Col>
-      <Col span={6}>col-6</Col>
-      <Col span={6}>col-6</Col>
-    </Row>
-  </>
+  <Row className='headerWrapper' align='middle'>
+    <Col span={24}>
+      <Row className='header' align='middle'>
+        <Col span={12}>
+          <Row className='leftHeader' align='middle'>
+            <Col className='logo' span={8}>
+              <img src='https://broniboy.ru/img/content/logo/main-logo.svg' alt='Broniboy' />
+            </Col>
+            <Col>
+              <svg
+                width='24'
+                height='26'
+                //   viewBox='0 0 24 26'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  d='M17.2764 7.02209V7.02209C20.1906 9.9363 20.1906 14.6612 17.2764 17.5754L13.2432 21.6086C12.5565 22.2953 11.443 22.2953 10.7563 21.6086L6.72311 17.5754C3.80889 14.6612 3.80889 9.9363 6.72311 7.02209V7.02209C9.63732 4.10788 14.3622 4.10788 17.2764 7.02209Z'
+                  stroke='black'
+                />
+                <path
+                  d='M17.2764 7.02209V7.02209C20.1906 9.9363 20.1906 14.6612 17.2764 17.5754L13.2432 21.6086C12.5565 22.2953 11.443 22.2953 10.7563 21.6086L6.72311 17.5754C3.80889 14.6612 3.80889 9.9363 6.72311 7.02209V7.02209C9.63732 4.10788 14.3622 4.10788 17.2764 7.02209Z'
+                  stroke='black'
+                />
+                <path
+                  d='M12 14.8593C13.4141 14.8593 14.5604 13.713 14.5604 12.2989C14.5604 10.8848 13.4141 9.73848 12 9.73848C10.5859 9.73848 9.43958 10.8848 9.43958 12.2989C9.43958 13.713 10.5859 14.8593 12 14.8593Z'
+                  stroke='black'
+                />
+              </svg>
+            </Col>
+            <Col className='logo' span={8}>
+              <span>Нижний Новгород</span>
+            </Col>
+          </Row>
+        </Col>
+        <Col span={12} className='rightHeader'>
+          <Row align='middle' justify='end'>
+            <Col className='headerCart'>
+              <span />
+            </Col>
+            <Col>
+              <Button>войти</Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Col>
+  </Row>
 );
