388caaeb690ad58603a9fe5b189aa0914ed8e86c
super Harry <harry-run@yandex.ru>
2022-02-06 14:06:12 +0300

    Meta fix

--- src/common/template/containers/Content/components/MetaContent/MetaContent.tsx
+++ src/common/template/containers/Content/components/MetaContent/MetaContent.tsx
@@ -1,107 +1,26 @@
 import React from 'react';
+import MetaBreadcrumsItems from './components/MetaItems/MetaBreadcrumsItems/MetaBreadcrumsItems';
+import MetaItemList from './components/MetaItems/MetaItemList/MetaItemList';
+import MetaOrganisationItems from './components/MetaItems/MetaOrganisationItems/MetaOrganisationItems';
+import MetaRatingItem from './components/MetaItems/MetaRatingItem/MetaRatingItem';
+import MetaSchemaWebPage from './components/MetaItems/MetaSchemaWebPage/MetaSchemaWebPage';
 import MetaSchema from './components/MetaItems/MetaSchemaWebPage/MetaSchemaWebPage';
 
 export const MetaContent = () => (
   <>
     <MetaSchema />
-    <div itemScope itemType='https://schema.org/BreadcrumbList' className='hidden'>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='Broniboy' />
-        <meta itemProp='position' />
-        <a itemProp='url' href='https://broniboy.ru/' />
-        <meta itemProp='item' content='https://broniboy.ru/' />
-      </div>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='Нижний Новгород' />
-        <meta itemProp='position' />
-        <a itemProp='url' href='https://broniboy.ru/nn/' />
-        <meta itemProp='item' content='https://broniboy.ru/nn/' />
-      </div>
-    </div>
-    <div itemScope itemType='https://schema.org/Organization' className='hidden'>
-      <meta itemProp='name' content='Сервис доставки Broniboy' />
-      <a itemProp='url' href='https://broniboy.ru/nn/' />
-      <meta itemProp='email' content='support@broniboy.ru' />
-      <meta itemProp='telephone' />
-      <meta itemProp='logo' content='https://broniboy.ru/img/content/logo/logo_squared.jpg' />
-      <meta itemProp='image' content='https://broniboy.ru/img/social/social.jpg' />
-      <meta itemProp='image' content='https://broniboy.ru/img/social/social_1_1.jpg' />
-      <meta itemProp='image' content='https://broniboy.ru/img/social/social_4_3.jpg' />
-      <meta
-        itemProp='address'
-        content='350000, Краснодарский край, г. Краснодар, ул. Рашпилевская, д. 110'
-      />
-      <div itemProp='aggregateRating' itemScope itemType='https://schema.org/AggregateRating'>
-        <meta itemProp='ratingValue' content='4.1' />
-        <meta itemProp='ratingCount' />
-        <meta itemProp='bestRating' />
-        <meta itemProp='worstRating' />
-      </div>
-    </div>
-    <div itemScope itemType='https://schema.org/ItemList' className='hidden'>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='Три апельсина' />
-        <meta itemProp='position' />
-        <a itemProp='url' href='https://broniboy.ru/nn/restaurants/p_tri-apelsina/' />
-      </div>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='Coffee Cake' />
-        <meta itemProp='position' />
-        <a itemProp='url' href='https://broniboy.ru/nn/restaurants/p_coffee-cake/' />
-      </div>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='Горячо' />
-        <meta itemProp='position' />
-        <a itemProp='url' href='https://broniboy.ru/nn/restaurants/p_goriacho/' />
-      </div>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='Маджонг' />
-        <meta itemProp='position' />
-        <a itemProp='url' href='https://broniboy.ru/nn/restaurants/p_madzhong/' />
-      </div>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='Юла Пицца' />
-        <meta itemProp='position' />
-        <a itemProp='url' href='https://broniboy.ru/nn/restaurants/p_iula-pitstsa/' />
-      </div>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='AMO' />
-        <meta itemProp='position' />
-        <a itemProp='url' href='https://broniboy.ru/nn/restaurants/p_amo/' />
-      </div>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='Хачапури Тетушки Марико' />
-        <meta itemProp='position' />
-        <a itemProp='url' href='https://broniboy.ru/nn/restaurants/p_khachapuri-tetushki-mariko/' />
-      </div>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='Та самая шаурма на Средном' />
-        <meta itemProp='position' />
-        <a
-          itemProp='url'
-          href='https://broniboy.ru/nn/restaurants/p_ta-samaia-shaurma-na-srednom/'
-        />
-      </div>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='Franky bar' />
-        <meta itemProp='position' />
-        <a itemProp='url' href='https://broniboy.ru/nn/restaurants/p_franky-bar/' />
-      </div>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='JAMEL' />
-        <meta itemProp='position' />
-        <a itemProp='url' href='https://broniboy.ru/nn/restaurants/p_chaikhana-zhamel/' />
-      </div>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='Ригла' />
-        <meta itemProp='position' />
-        <a itemProp='url' href='https://broniboy.ru/nn/pharmacy/p_rigla/' />
-      </div>
-      <div itemProp='itemListElement' itemScope itemType='https://schema.org/ListItem'>
-        <meta itemProp='name' content='Хачапури и Вино' />
-        <meta itemProp='position' />
-        <a itemProp='url' href='https://broniboy.ru/nn/restaurants/p_khachapuri-i-vino/' />
-      </div>
-    </div>
+    <MetaSchemaWebPage />
+    <MetaRatingItem />
+    <MetaBreadcrumsItems
+      metaElements={[
+        {
+          breadcrumbUrlPart: 'http:vk.com',
+          name: 'MeForever',
+        },
+      ]}
+    />
+
+    <MetaOrganisationItems />
+    <MetaItemList />
   </>
 );
