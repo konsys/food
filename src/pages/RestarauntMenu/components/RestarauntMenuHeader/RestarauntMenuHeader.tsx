a6502b15bfb1de432e24d5f2f53bc8fc14f5cd84
super Harry <harry-run@yandex.ru>
2022-02-07 18:09:03 +0300

    Rating

--- src/pages/RestarauntMenu/components/RestarauntMenuHeader/RestarauntMenuHeader.tsx
+++ src/pages/RestarauntMenu/components/RestarauntMenuHeader/RestarauntMenuHeader.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RatingDto } from '../../../../common/types/dto';
+import RatingComponent from '../../../../common/template/components/RatingComponent/RatingComponent';
 
 interface Props {
   restaruantImgSrc: string;
@@ -9,23 +10,11 @@ interface Props {
 
 function RestarauntMenuHeader(props: Props) {
   const { restarauntName, restaruantImgSrc, rating } = props;
-  rating;
-  restaruantImgSrc;
   return (
     <section className='restaurant restaurant--shop' style={{ marginBottom: 0 }}>
       <div className='restaurant-content'>
         <div className='restaurant__background'>
-          <img
-            src=''
-            className='restaurant-bg-image'
-            alt={restarauntName}
-            style={{
-              height: 'auto',
-              width: 840,
-              marginLeft: 0,
-              marginTop: '-78.75px',
-            }}
-          />
+          <img src={restaruantImgSrc} className='restaurant-bg-image' alt={restarauntName} />
         </div>
         <div className='restaurant-descr'>
           <h1>
@@ -33,19 +22,7 @@ function RestarauntMenuHeader(props: Props) {
           </h1>
           <i className='restaurant-descr__bull'>&nbsp;•&nbsp;</i>
           <span className='restaurant-box-second__rating' style={{ color: '#76C032' }}>
-            <strong>{rating}&nbsp;</strong>
-            <svg
-              width={38}
-              height={37}
-              viewBox='0 0 38 37'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path
-                d='M18.0489 0.927049C18.3483 0.00573802 19.6517 0.00574017 19.9511 0.927051L23.7148 12.5106C23.8487 12.9227 24.2326 13.2016 24.6659 13.2016H36.8456C37.8143 13.2016 38.2171 14.4412 37.4333 15.0106L27.5798 22.1697C27.2293 22.4243 27.0826 22.8757 27.2165 23.2877L30.9802 34.8713C31.2796 35.7926 30.2251 36.5588 29.4414 35.9894L19.5878 28.8303C19.2373 28.5757 18.7627 28.5757 18.4122 28.8303L8.55862 35.9894C7.77491 36.5588 6.72043 35.7926 7.01978 34.8713L10.7835 23.2877C10.9174 22.8757 10.7707 22.4243 10.4202 22.1697L0.566653 15.0106C-0.217061 14.4412 0.185717 13.2016 1.15444 13.2016H13.3342C13.7674 13.2016 14.1513 12.9227 14.2852 12.5106L18.0489 0.927049Z'
-                fill='#76C032'
-              />
-            </svg>
+            <RatingComponent rating={rating} />
           </span>
           <div className='restaurant-info restaurant-info--shop'>
             <div className='restaurant-info__delivery'>
