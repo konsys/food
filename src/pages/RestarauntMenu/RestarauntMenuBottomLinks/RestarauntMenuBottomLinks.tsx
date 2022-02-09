81dc7e0c67deb6247ff15ad7e57178ab5a7cf524
super Harry <harry-run@yandex.ru>
2022-02-09 17:53:49 +0300

    Меню ссылки

--- src/pages/RestarauntMenu/RestarauntMenuBottomLinks/RestarauntMenuBottomLinks.tsx
+++ src/pages/RestarauntMenu/RestarauntMenuBottomLinks/RestarauntMenuBottomLinks.tsx
@@ -1,165 +1,62 @@
 import React, { memo } from 'react';
+import { TLinkWithText } from '../../../common/types/utilTypes';
+import RestarauntMenuBottomLinksItem from './components/RestarauntMenuBottomLinksItem';
 
 interface Props {}
+
+const items: TLinkWithText[] = [
+  {
+    link: '/',
+    text: 'Обеды',
+  },
+  {
+    link: '/',
+    text: 'Салаты',
+  },
+  {
+    link: '/',
+    text: 'Основные блюда',
+  },
+  {
+    link: '/',
+    text: 'Стритфуд',
+  },
+  {
+    link: '/',
+    text: 'Супы',
+  },
+  {
+    link: '/',
+    text: 'Паста',
+  },
+  {
+    link: '/',
+    text: 'Десерты',
+  },
+  {
+    link: '/',
+    text: 'Завтраки',
+  },
+  {
+    link: '/',
+    text: 'Мясо',
+  },
+  {
+    link: '/',
+    text: 'Чизкейки',
+  },
+  {
+    link: '/',
+    text: 'Европейская кухня',
+  },
+];
 
 function RestarauntMenuBottomLinks(props: Props) {
   const {} = props;
 
   return (
     <div>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Бизнес-ланчи'
-        href='https://broniboy.ru/nn/restaurants/business-lunch/'
-      >
-        Бизнес-ланчи
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Европейская кухня'
-        href='https://broniboy.ru/nn/restaurants/evropeiskaia-kukhnia/'
-      >
-        Европейская кухня
-      </a>
-      <a style={{ paddingRight: 20 }} title='Мясо' href='https://broniboy.ru/nn/restaurants/meat/'>
-        Мясо
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Японская кухня'
-        href='https://broniboy.ru/nn/restaurants/iaponskaia-kukhnia/'
-      >
-        Японская кухня
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Здоровая еда'
-        href='https://broniboy.ru/nn/restaurants/healthy-food/'
-      >
-        Здоровая еда
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Торты'
-        href='https://broniboy.ru/nn/restaurants/cakes/'
-      >
-        Торты
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Десерты'
-        href='https://broniboy.ru/nn/restaurants/desserts/'
-      >
-        Десерты
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Стейки'
-        href='https://broniboy.ru/nn/restaurants/steaks/'
-      >
-        Стейки
-      </a>
-      <a style={{ paddingRight: 20 }} title='Супы' href='https://broniboy.ru/nn/restaurants/soups/'>
-        Супы
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Рыба и морепродукты'
-        href='https://broniboy.ru/nn/restaurants/fish/'
-      >
-        Рыба и морепродукты
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Бургеры'
-        href='https://broniboy.ru/nn/restaurants/burgers/'
-      >
-        Бургеры
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Гриль'
-        href='https://broniboy.ru/nn/restaurants/grill/'
-      >
-        Гриль
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Основные блюда'
-        href='https://broniboy.ru/nn/restaurants/main/'
-      >
-        Основные блюда
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Азиатская кухня'
-        href='https://broniboy.ru/nn/restaurants/aziatskaia-kukhnia/'
-      >
-        Азиатская кухня
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Суши и роллы'
-        href='https://broniboy.ru/nn/restaurants/sushi/'
-      >
-        Суши и роллы
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Паста'
-        href='https://broniboy.ru/nn/restaurants/pasta/'
-      >
-        Паста
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Чизкейки'
-        href='https://broniboy.ru/nn/restaurants/cheesecakes/'
-      >
-        Чизкейки
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='На день рождения'
-        href='https://broniboy.ru/nn/restaurants/happy-birthday/'
-      >
-        На день рождения
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Обеды'
-        href='https://broniboy.ru/nn/restaurants/lunch/'
-      >
-        Обеды
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Курица гриль'
-        href='https://broniboy.ru/nn/restaurants/chicken-grill/'
-      >
-        Курица гриль
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Завтраки'
-        href='https://broniboy.ru/nn/restaurants/breakfast/'
-      >
-        Завтраки
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Стритфуд'
-        href='https://broniboy.ru/nn/restaurants/streetfood/'
-      >
-        Стритфуд
-      </a>
-      <a
-        style={{ paddingRight: 20 }}
-        title='Салаты'
-        href='https://broniboy.ru/nn/restaurants/salads/'
-      >
-        Салаты
-      </a>
+      <RestarauntMenuBottomLinksItem items={items} />
     </div>
   );
 }
---
+++ src/pages/RestarauntMenu/RestarauntMenuBottomLinks/components/RestarauntMenuBottomLinksItem.tsx
@@ -0,0 +1,22 @@
+import React, { memo } from 'react';
+import { TLinkWithText } from '../../../../common/types/utilTypes';
+
+interface Props {
+  items: TLinkWithText[];
+}
+
+function RestarauntMenuBottomLinksItem(props: Props) {
+  const { items } = props;
+
+  return (
+    <>
+      {items.map(({ link, text }) => (
+        <a style={{ paddingRight: 20 }} title={text} href={link}>
+          {text}
+        </a>
+      ))}
+    </>
+  );
+}
+
+export default memo(RestarauntMenuBottomLinksItem);
