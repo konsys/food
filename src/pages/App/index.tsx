f8ed37f77c2b2dae79c9c947f3fc85d3e7d196e8
super Harry <harry-run@yandex.ru>
2022-02-09 17:36:52 +0300

    Colors

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes } from 'react-router-dom';
 import { Template } from '../../common/template';
 import { getRoutes } from '../../routes';
-// import './style/app.style.less';
 import '../../config/theme/antd-theme.less';
 
 function App() {
