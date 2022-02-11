f9dba3e7ea79c39ae2dad4c9e9f44f96b5195450
super Harry <harry-run@yandex.ru>
2022-02-11 18:27:18 +0300

    wefwef

--- src/common/template/containers/Header/components/HeaderLogo/HeaderLogo.tsx
+++ src/common/template/containers/Header/components/HeaderLogo/HeaderLogo.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 export const HeaderLogo = () => (
   <div className='header-logo'>
-    <a href='https://broniboy.ru' title='Главная'>
-      <img
+    <a href='https://broniboy.ru' className='d-sm-block d-md-none' title='Главная'>
+      {/* <img
         src='https://broniboy.ru/img/content/logo/main-logo.svg'
         alt='Broniboy'
         className='hidden-xs'
-      />
+      /> */}
       <img
         src='https://broniboy.ru/img/content/logo/main-logo-mobile.svg'
         alt='Broniboy'
-        className='visible-xs'
+        className='d-sm-none d-md-block'
+        style={{ height: '50px' }}
       />
     </a>
   </div>
