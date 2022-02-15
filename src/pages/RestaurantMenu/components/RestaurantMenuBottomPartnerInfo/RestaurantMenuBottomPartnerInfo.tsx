a6495c72812c0c19febaa7fa81b0c89719e769f7
super Harry <harry-run@yandex.ru>
2022-02-15 15:51:34 +0300

    Fix legal

--- src/pages/RestaurantMenu/components/RestaurantMenuBottomPartnerInfo/RestaurantMenuBottomPartnerInfo.tsx
+++ src/pages/RestaurantMenu/components/RestaurantMenuBottomPartnerInfo/RestaurantMenuBottomPartnerInfo.tsx
@@ -14,10 +14,12 @@ function RestaurantMenuBottomPartnerInfo(props: Props) {
 
   return (
     <div className='legal-info'>
-      Исполнитель: {partnerName}, Адрес: {partnerAddress}
-      <span className='legal-info__part'>ИНН {partnerINN}</span>,
-      <span className='legal-info__part'>КПП {partnerKPP}</span>,
-      <span className='legal-info__part'>ОГРН {partnerOGRN}</span>
+      <span className='legal-info__part'>
+        Исполнитель: {partnerName}, Адрес: {partnerAddress},
+      </span>
+      <span className='legal-info__part'>ИНН {partnerINN}</span>
+      {partnerKPP && <span className='legal-info__part'>КПП {partnerKPP}</span>}
+      {partnerOGRN && <span className='legal-info__part'>ОГРН {partnerOGRN}</span>}
     </div>
   );
 }
