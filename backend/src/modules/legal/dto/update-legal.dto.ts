642cb320f2008be6a388d08d67a77a7ed466479c
super Harry <harry-run@yandex.ru>
2022-02-20 18:40:03 +0300

    Fix

--- backend/src/modules/legal/dto/update-legal.dto.ts
+++ backend/src/modules/legal/dto/update-legal.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType } from "@nestjs/mapped-types";
 import { CreateLegalDto } from './create-legal.dto';
 
 export class UpdateLegalDto extends PartialType(CreateLegalDto) {}
--- backend/src/modules/restaurant/dto/create-restaurant.dto.ts
+++ backend/src/modules/restaurant/dto/create-restaurant.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType } from "@nestjs/mapped-types";
 import { Restaurant } from "src/entities/restaurant.entity";
 
 export class CreateRestaurantDto extends PartialType(Restaurant) {}
