592698dcffb64d3f579f44c53cf5747c6b451dcf
super Harry <harry-run@yandex.ru>
2022-02-23 14:28:57 +0300

    Order

--- src/pages/RestaurantMenu/components/RestaurantMenuListBlock/RestaurantMenuListBlock.tsx
+++ src/pages/RestaurantMenu/components/RestaurantMenuListBlock/RestaurantMenuListBlock.tsx
@@ -1,4 +1,6 @@
 import React, { memo } from 'react';
+import { chain } from 'lodash';
+
 import { RestaurantMenuDto } from '../../../../modules/restaurantMenu/types';
 import RestaurantMenuListItem from './components/RestaurantMenuListItem';
 import './restaurantMenuListBlock.less';
@@ -10,17 +12,27 @@ interface Props {
 function RestaurantMenuListBlock(props: Props) {
   const { menu } = props;
 
+  const grouppedByCategory = chain(menu)
+    .groupBy('foodCategory.name')
+    // .orderBy()
+    .map((value, key) => ({ title: key, menu: value }))
+    .value();
+
   return (
     <>
-      <div className='restaurant-menu__section-title'>
-        <h2>Суши</h2>
-      </div>
-      <div className='restaurant-menu__service-list row'>
-        {menu.map((item, index) => (
-          <RestaurantMenuListItem item={item} key={index} />
-        ))}
-      </div>
-      <div className='service-list' />
+      {grouppedByCategory.map((v) => (
+        <>
+          <div className='restaurant-menu__section-title'>
+            <h2>{v.title}</h2>
+          </div>
+          <div className='restaurant-menu__service-list row'>
+            {v.menu.map((item, index) => (
+              <RestaurantMenuListItem item={item} key={index} />
+            ))}
+          </div>
+          <div className='service-list' />
+        </>
+      ))}
     </>
   );
 }
