03dd4f027b2f1de25880677f36bc396a7d735269
super Harry <harry-run@yandex.ru>
2022-02-24 10:51:54 +0300

    wefwef

--- backend/src/entities/cart.entity.ts
+++ backend/src/entities/cart.entity.ts
@@ -1,1 +1,25 @@
-export class Cart {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Cart {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    uuid: string;
+  
+    @Column()
+    name: string;
+  
+    @Column({default: null})
+    description: string;
+
+    @Column()
+    clientUuid: string;
+
+//     @Column({
+//         type: 'jsonb'
+//       })
+//     public properties: CarProperties;
+}
+
