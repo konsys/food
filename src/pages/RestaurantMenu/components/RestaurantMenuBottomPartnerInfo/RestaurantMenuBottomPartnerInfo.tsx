91a45b2de8effc3a2a14e9827cde3db721593b98
super Harry <harry-run@yandex.ru>
2022-03-04 10:05:13 +0300

    Resp

--- src/pages/RestaurantMenu/components/RestaurantMenuBottomPartnerInfo/RestaurantMenuBottomPartnerInfo.tsx
+++ src/pages/RestaurantMenu/components/RestaurantMenuBottomPartnerInfo/RestaurantMenuBottomPartnerInfo.tsx
@@ -8,20 +8,20 @@ type Prop = {
 
 function RestaurantMenuBottomPartnerInfo({ legal }: Prop) {
   return (
-    <>
+    <div>
       {legal ? (
-        <div className='legal-info'>
-          <span className='legal-info__part'>
+        <div className='legal-info d-flex flex-md-row'>
+          <div className='legal-info__part'>
             Исполнитель: {legal.name}, Адрес: {legal.address},
-          </span>
-          <span className='legal-info__part'>ИНН {legal.inn}</span>
-          {legal.kpp && <span className='legal-info__part'>КПП {legal.kpp}</span>}
-          {legal.ogrn && <span className='legal-info__part'>ОГРН {legal.ogrn}</span>}
+          </div>
+          <div className='legal-info__part'>ИНН {legal.inn}</div>
+          {legal.kpp && <div className='legal-info__part'>КПП {legal.kpp}</div>}
+          {legal.ogrn && <div className='legal-info__part'>ОГРН {legal.ogrn}</div>}
         </div>
       ) : (
         ''
       )}
-    </>
+    </div>
   );
 }
 
