0329695cd66d0774d2a342a16c29b89190661ff2
konsys <harry-run@yandex.ru>
2022-03-20 10:34:20 +0300

    wefwefwe

--- src/modules/restaurantMenu/__tests__/restaurantsMenu.test.ts
+++ src/modules/restaurantMenu/__tests__/restaurantsMenu.test.ts
@@ -1,17 +1,20 @@
-import { RestaurantMenuModel } from '../../../store';
+import { RestaurantMenuModel, RestaurantModel } from '../../../store';
 import { restaurantMenuFactory } from '../restaurantMenuFactory';
 
 describe('menu tests', () => {
   const { createItemFx } = RestaurantMenuModel;
-
-  const generateNewItem = () => restaurantMenuFactory.build();
+  const { getItemByFilterFx } = RestaurantModel;
 
   it('should create menu', async () => {
-    for (let i = 0; i < 1000; i++) {
-      const item = generateNewItem();
-      await Promise.all(new Array(20).fill(await createItemFx(item)));
-    }
+    for (let i = 0; i < 10; i++) {
 
+      // for (let i1 = 1; i1 < 11; i1++) {
+      // const item = restaurantMenuFactory.build({ restaurantId: i1 });
+      const restaraunt = await getItemByFilterFx({})
+      const item = restaurantMenuFactory.build();
+      await createItemFx(item);
+      // }
+    }
     expect(1).toBe(1);
   });
 });
