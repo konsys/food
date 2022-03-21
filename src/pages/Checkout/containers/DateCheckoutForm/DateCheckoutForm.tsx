ef511eeb4a66b068622c4bcdceca2f6ec3be33bf
konsys <harry-run@yandex.ru>
2022-03-21 20:03:47 +0300

    sdvwefwef

--- src/pages/Checkout/containers/DateCheckoutForm/DateCheckoutForm.tsx
+++ src/pages/Checkout/containers/DateCheckoutForm/DateCheckoutForm.tsx
@@ -1,17 +1,19 @@
 import DateTimePicker from 'react-datetime-picker';
 import moment from 'moment';
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 
 interface Props {}
 
 function DateCheckoutForm(props: Props) {
   const {} = props;
 
+  const [time, setTime] = useState<Date>(moment().toDate());
+
   return (
     <div className='ordering-form__time--input'>
       <label>Время бронирования</label>
       <div className='order-options-time'>
-        <DateTimePicker onChange={() => null} value={moment().toDate()} />
+        <DateTimePicker onChange={setTime} value={time} />
       </div>
     </div>
   );
