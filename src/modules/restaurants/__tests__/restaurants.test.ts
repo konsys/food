e937d820793a0445882f8442377f3ef5b7ca5460
konsys <harry-run@yandex.ru>
2022-03-22 21:21:14 +0300

    wefwef

--- src/modules/restaurants/__tests__/restaurants.test.ts
+++ src/modules/restaurants/__tests__/restaurants.test.ts
@@ -3,14 +3,14 @@ import { restaurantFactory } from '../restaurantFactory';
 
 const { createItemFx } = RestaurantModel;
 
-const generateNewItem = () => restaurantFactory.build();
 
 describe('menu tests', () => {
   it('should create menu', async () => {
     for (let i = 0; i < 10; i++) {
-      const item = generateNewItem();
+      const item = restaurantFactory.build();
+      await createItemFx(item);
       expect(item).toBe(1);
-      await Promise.all(new Array(10).fill(await createItemFx(item)));
+
     }
 
     expect(1).toBe(1);
