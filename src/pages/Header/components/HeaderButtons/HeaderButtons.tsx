ad3866351f3a9bb26da2233c88792385f129a782
konsys <harry-run@yandex.ru>
2022-04-13 10:04:54 +0300

    sdvsdv

--- src/pages/Header/components/HeaderButtons/HeaderButtons.tsx
+++ src/pages/Header/components/HeaderButtons/HeaderButtons.tsx
@@ -1,3 +1,4 @@
+import { Button } from 'antd';
 import React, { memo } from 'react';
 import { Link } from 'react-router-dom';
 import { TItemWithUuid } from '../../../../common/types';
@@ -9,28 +10,32 @@ type Props = {
   cart?: Nullable<TItemWithUuid<CartDto>>;
 };
 const HeaderButtons = ({ cart }: Props) => (
-  <nav className='header-nav header-buttons d-flex float-end'>
-    <ul className='list-clear clearfix'>
-      <li className='d-none d-md-flex '>
+  <nav className="header-nav header-buttons d-flex float-end">
+    <ul className="list-clear clearfix">
+      <li className="d-none d-md-flex ">
         {cart?.orderSum ? (
           <Link
             to={`/cart/${cart?.uuid}`}
-            title='Корзина'
-            rel='nofollow'
-            className='header-nav-item-link-basket header-nav-item-link-basket_active header-nav-item-link-basket_active-animate'
+            title="Корзина"
+            rel="nofollow"
+            className="header-nav-item-link-basket header-nav-item-link-basket_active header-nav-item-link-basket_active-animate"
           >
             <b>{cart?.orderSum} ₽</b>
           </Link>
         ) : (
-          <span title='Корзина пуста' className='header-nav-item-link-basket-stub' />
+          <span
+            title="Корзина пуста"
+            className="header-nav-item-link-basket-stub"
+          />
         )}
       </li>
       <li>
-        <Link to='/' className='header-nav-item-link' title='Войти'>
+        <Button className="header-nav-item-link__login" title="Войти">
           <span>Войти</span>
-        </Link>
+        </Button>
       </li>
     </ul>
+    {/* <StandartModal /> */}
   </nav>
 );
 
