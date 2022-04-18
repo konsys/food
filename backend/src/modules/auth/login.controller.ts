3bbac1c1ef9f02132abd7ea35e74eb9fed455dcf
konsys <harry-run@yandex.ru>
2022-04-18 21:16:34 +0300

    wefwef

--- backend/src/modules/auth/login.controller.ts
+++ backend/src/modules/auth/login.controller.ts
@@ -1,13 +1,21 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local.auth.guard';
 
 @UseGuards(LocalAuthGuard)
 @Controller()
 export class LoginController {
+    constructor(
+        private readonly authService: AuthService,
+    ) { }
+
     @UseGuards(AuthGuard('local'))
     @Post('/users/auth/login')
     async login(@Request() req) {
-        return req.user;
+        return this.authService.login({
+            name: req.user.name,
+            userUuid: req.user.userUuid,
+        });
     }
 }
--- backend/src/modules/auth/users.module.ts
+++ backend/src/modules/auth/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/config';
+import { AuthService } from './auth.service';
 import { LoginController } from './login.controller';
 import { UsersService } from './users.service';
 
 @Module({
-    providers: [UsersService],
+    imports: [JwtModule.register({
+        secret: jwtConstants.secret,
+        signOptions: { expiresIn: jwtConstants.expires },
+    })],
+    providers: [UsersService, AuthService],
     exports: [UsersService],
     controllers: [LoginController]
 })
