a73ef4f8a5ee0a8f3577714606923bbfa3b73ffa
konsys <harry-run@yandex.ru>
2022-04-18 21:01:30 +0300

    ergerg

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -4,9 +4,13 @@ import { UsersModule } from './users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/config';
 
 @Module({
-    imports: [UsersModule, PassportModule],
+    imports: [UsersModule, PassportModule, JwtModule.register({
+        secret: jwtConstants.secret,
+        signOptions: { expiresIn: jwtConstants.expires },
+    }),],
     providers: [AuthService, LocalStrategy],
     exports: [AuthService],
 })
