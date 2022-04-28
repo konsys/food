98dc7575bd5a2950fd3334eba338de790407e184
konsys <harry-run@yandex.ru>
2022-04-28 18:26:21 +0300

    sdvsdv

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,16 +1,21 @@
 import React, { memo } from 'react';
 import { Routes } from 'react-router-dom';
+import { useGate } from 'effector-react';
 import { Template } from '../../common/template';
 import { getRoutes } from '../../routes';
 import '../../config/theme/antd-theme.less';
 import 'moment/locale/ru';
 
 import './style/app.style.less';
+import { ProfileGate } from '../../modules/user/store';
 
-const App = () => (
-  <Template>
-    <Routes>{getRoutes()}</Routes>
-  </Template>
-);
+const App = () => {
+  useGate(ProfileGate);
+  return (
+    <Template>
+      <Routes>{getRoutes()}</Routes>
+    </Template>
+  );
+};
 
 export default memo(App);
