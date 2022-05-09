fdc96056a1ac0d28f5ece3352aa0703f282c9d6e
konsys <harry-run@yandex.ru>
2022-05-09 12:27:28 +0300

    sdvsdv

--- src/pages/Cart/components/CartItem/CartItem.tsx
+++ src/pages/Cart/components/CartItem/CartItem.tsx
@@ -1,10 +1,12 @@
 import React, { memo } from 'react';
+import { Row, Col } from 'antd';
 import { TUuid, TVoidFn } from '../../../../common/types';
 import { NullableNumber } from '../../../../core/types';
 import { RestaurantMenuDto } from '../../../../modules/restaurantMenu/types';
 import { ReactComponent as DeleteButton } from './svg/deleteButton.svg';
 import { ReactComponent as PlusButton } from './svg/plusButton.svg';
 import { ReactComponent as MinusButton } from './svg/minusButton.svg';
+import './cartItem.less';
 
 export type TModidificator = {
   price: NullableNumber;
@@ -24,48 +26,49 @@ function CartItem(props: Props) {
     props;
 
   return (
-    <div className="cart-service">
-      <div className="cart-service__title d-flex flex-wrap align-items-center">
-        <button
-          className="cart-service__delete-button"
-          type="button"
-          onClick={() => deleteFromCart(item.uuid)}
-        >
-          <DeleteButton />
-        </button>
-        <div className="cart-service__name">{item.name}</div>
-        <div className="cart-service__description">{item.weight} г.</div>
-      </div>
-      <div className="cart-service__addons">
-        {modificators?.map(({ key, value, price }) => {
-          const priceAdd = price ? ` <strong> (+ ${price}₽)</strong>` : '';
-          return `${key}: ${value} ${priceAdd}`;
-        })}
-        <br />
-      </div>
-      <div className="cart-service__bottom">
-        <div className="cart-service__actions">
+    <div className="cart-item">
+      <Row>
+        <Col span={10} className="cart-item__title">
+          {item.name}
+        </Col>
+        <Col span={10} className="cart-item__description">
+          {item.weight} г.
+        </Col>
+        <Col span={2}>
           <button
-            className="cart-service__count-button cart-service__count-button--minus"
+            className="cart-item__delete-button"
+            type="button"
+            onClick={() => deleteFromCart(item.uuid)}
+          >
+            <DeleteButton />
+          </button>
+        </Col>
+        <Col span={2}>
+          <button
+            className="cart-item__count-button"
+            type="button"
+            onClick={() => changeQuantity(item.uuid, 1)}
+          >
+            <PlusButton />
+          </button>
+        </Col>
+        <Col span={24}>
+          <input type="text" value={quiantity} disabled />
+        </Col>
+        <Col span={24}>
+          <button
+            className="cart-item__count-button cart-item__count-button--minus"
             disabled={quiantity < 2}
             type="button"
             onClick={() => changeQuantity(item.uuid, -1)}
           >
             <MinusButton />
           </button>
-          <input type="text" value={quiantity} disabled />
-          <button
-            className="cart-service__count-button"
-            type="button"
-            onClick={() => changeQuantity(item.uuid, 1)}
-          >
-            <PlusButton />
-          </button>
-        </div>
-        <div className="cart-service__price">
+        </Col>
+        <Col span={24}>
           <span>{item.price * quiantity}</span> ₽
-        </div>
-      </div>
+        </Col>
+      </Row>
     </div>
   );
 }
